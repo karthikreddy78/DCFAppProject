53483838b064f57ea8384f8f0cf82ca185ab589c
Reddy Musku <mkarthikreddy78@gmail.com>
2021-12-06 15:57:38 +0530

    Added Microservices

---
+++ DCF-backend/order/order/src/test/java/com/musku/order/OrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.musku.order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
