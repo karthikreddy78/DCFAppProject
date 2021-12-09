5e1bd065d1bb7654c7da085d02fc0d31d402789a
karthikreddy78 <karthik255>
2021-12-10 00:28:11 +0530

    Added

---
+++ DCF-backend/api-gateway/api-gateway/src/main/java/com/musku/apigateway/HelloWorldController.java
@@ -0,0 +1,15 @@
+package com.musku.apigateway;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @RequestMapping("/hello")
+    public String hello()
+    {
+        return "hello";
+    }
+}
